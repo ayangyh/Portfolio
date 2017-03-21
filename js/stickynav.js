fc31ad8cdf478c33f48b3817e5725d25e32a4b98
Angela Yang <angelayangyh@gmail.com>
2017-03-21 11:48:03 -0700

    case navbar sticky fixed

--- js/stickynav.js
+++ js/stickynav.js
@@ -1,15 +1,20 @@
-   	$(document).ready(function(){
+$(document).ready(function(){
 
-    var nb = $(".navbar");
-    var tall = $(window).height();
-    var nbheight = $(".navbar").height();
-    var stick = tall - nbheight;
+  var nb = $(".navbar");
+  var tall = $(window).height();
+  var nbheight = $(".navbar").height();  
+  var stick = tall - nbheight;
 
-    $(window).scroll(function() {
-      if( $(this).scrollTop() > stick -50){
-        nb.addClass("navbar-scrolled");
-      }
-      else {
-        nb.removeClass("navbar-scrolled");
-      }
-    });
+  console.log(stick);
+
+
+  $(window).scroll(function() {
+    if( $(this).scrollTop() > stick - 20){
+      nb.addClass("navbar-scrolled");
+    }
+    else {
+      nb.removeClass("navbar-scrolled");
+    }
+  });
+
+});
